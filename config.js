27f1c9b3d8980dd67d818728c213452808f92804
klassresa5a <ilanit@bk.ru>
2025-11-20 17:49:08 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // === KLASSRESA 5A – KONFIGURATION ===
 window.KLASSRESA_CONFIG = {
   goal: 70000,
-  current: 17358,
+  current: 18200,
   ticks: [0, 10000, 15000, 20000, 30000, 70000],
   activities: [
     {
