aa1fdb15add54dd835f04a1b12186493b479d4e9
klassresa5a <ilanit@bk.ru>
2026-03-06 22:28:02 +0100

    Fix quote style in news title

--- config.js
+++ config.js
@@ -57,7 +57,7 @@ window.KLASSRESA_CONFIG = {
   news: [
     {
       date: "2026-03-22",
-      title: "Filmkväll – "A Minecraft Movie"",
+      title: 'Filmkväll – "A Minecraft Movie"',
       text: "Den 22 mars ordnar vi en mysig filmkväll med film, popcorn och chips. Kvällen är en belöning till eleverna för deras fantastiska insats i försäljningen och insamlingen till klassresan.",
       //albumUrl: "https://photos.app.goo.gl/rBWYv1WB2FGBfZjt6" // 👈 сюда вставь реальную ссылку
     }
